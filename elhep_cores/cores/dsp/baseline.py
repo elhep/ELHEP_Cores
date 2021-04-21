3d7cb38b445345668bb8beb4ccc2d43d929cc517
Michał Kuklewski <michael.kuklewski@gmail.com>
2021-04-21 18:52:27 +0200

    Fixed baseline

--- elhep_cores/cores/dsp/baseline.py
+++ elhep_cores/cores/dsp/baseline.py
@@ -16,7 +16,7 @@ import numpy as np
 
 
 class SignalBaseline(Module):
-    def __init__(self, wsize=16, fs=100000000.0, cutoff=10000.0, trans_width=10000.0, numtaps=266):
+    def __init__(self, wsize=16, fs=100000000.0, cutoff=10000.0, trans_width=1000000.0, numtaps=266):
         """Signal baseline computation module
 
         This is basically LPF designed with the given parameters.
@@ -31,7 +31,7 @@ class SignalBaseline(Module):
             numtaps (int, optional): Size of the FIR filter. Defaults to 266.
         """
         # Compute filter coefficients with SciPy
-        coef = signal.remez(numtaps, [0, cutoff, cutoff + trans_width, 0.5 * fs], [1, 0], Hz=fs)
+        coef = signal.remez(numtaps, [0, cutoff, cutoff + trans_width, 0.5 * fs], [1, 0], fs=fs)
 
         self.coef = coef
         self.wsize = wsize
@@ -51,3 +51,41 @@ class SignalBaseline(Module):
         sum_full = Signal((2 * self.wsize - 1, True))
         self.sync += sum_full.eq(reduce(add, muls))
         self.comb += self.o.eq(sum_full >> self.wsize - 1)
+
+      
+class SimulationWrapper(Module):
+
+    def __init__(self):
+
+        self.clock_domains.cd_sys = cd_sys = ClockDomain()
+
+        self.io = [
+            cd_sys.clk,
+            cd_sys.rst
+        ]
+
+        self.submodules.dut = dut = SignalBaseline()
+                                         
+        self.io += [    
+            dut.o,
+            dut.i
+        ]
+
+
+
+if __name__ == "__main__":
+    
+    # Generate simulation source for Cocotb
+    from migen.build.xilinx import common
+    from elhep_cores.simulation.common import update_tb
+
+    module = SimulationWrapper()
+    so = dict(common.xilinx_special_overrides)
+    so.update(common.xilinx_s7_special_overrides)
+
+    verilog.convert(fi=module,
+                    name="top",
+                    special_overrides=so,
+                    ios={*module.io},
+                    create_clock_domains=True).write('dut.v')
+    update_tb('dut.v')
