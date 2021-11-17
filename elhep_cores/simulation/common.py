edba9f958ef7b4abb478bfe6c62caa94780f0026
Mikolaj Sowinski <mikolaj.sowinski@gmail.com>
2021-11-17 19:45:35 +0100

    Added RTIO signals generation for simulation with Verilog

--- elhep_cores/simulation/common.py
+++ elhep_cores/simulation/common.py
@@ -43,9 +43,43 @@ def get_stream_signals(endpoint):
     return ios
 
 
+def get_rtio_signals(rtlink, link_name=None):
+    ios = set()
+    if hasattr(rtlink, "i"):
+        ios.add(rtlink.i.stb)
+        if link_name:
+            rtlink.i.stb.name_override = f"{link_name}_stb_o"
+        if hasattr(rtlink.i, "data"):
+            ios.add(rtlink.i.data)
+            if link_name:
+                rtlink.i.data.name_override = f"{link_name}_data_o"
+
+    if hasattr(rtlink, "o"):
+        if hasattr(rtlink.o, "address"):
+            ios.add(rtlink.o.address)
+            if link_name:
+                rtlink.o.address.name_override = f"{link_name}_address_i"
+        if hasattr(rtlink.o, "data"):
+            ios.add(rtlink.o.data)
+            if link_name:
+                rtlink.o.data.name_override = f"{link_name}_data_i"
+        ios.add(rtlink.o.stb)
+        if link_name:
+            rtlink.o.stb.name_override = f"{link_name}_stb_i"
+    if link_name:
+        for signal in ios:
+            print(signal.__dict__)
+    return ios
+
+
 def get_ios(f):
     """Finds all IOs following _i/_o suffixes and includes sink/source fields"""
     ios = {getattr(f, x) for x in f.__dict__.keys() if x.endswith("_o") or x.endswith("_i")}
+    return ios
+
+
+def get_all_ios(f):
+    ios = get_ios(f)
     if hasattr(f, "source"):
         ios = ios.union(get_stream_signals(f.source))
     if hasattr(f, "sink"):
@@ -53,7 +87,9 @@ def get_ios(f):
     return ios
 
 
-def generate_verilog(f, output_file):
-    verilog.convert(f, get_ios(f)).write(output_file)
+def generate_verilog(f, output_file, ios=None):
+    if ios is None:
+        ios = get_all_ios(f)
+    verilog.convert(f, ios, display_run=True).write(output_file)
     update_tb(output_file)
 
